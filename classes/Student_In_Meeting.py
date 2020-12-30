956c1739bde4f6ff94369b1bb4eb90725659ec57
Illya Verbovskyi <illyaverb@gmail.com>
2020-12-30 18:14:58 +0200

    Fix chrome options

--- classes/Student_In_Meeting.py
+++ classes/Student_In_Meeting.py
@@ -1,6 +1,7 @@
 import requests as req
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import NoSuchElementException
 
 import re
@@ -8,8 +9,19 @@ import re
 
 class Student_In_Meeting:
     def __init__(self):
-        self.driver = webdriver.Chrome()
-        self.driver.implicitly_wait(10)
+        opt = Options()
+        opt.add_argument("--disable-infobars")
+        opt.add_argument("start-maximized")
+        opt.add_argument("--disable-extensions")
+        # Pass the argument 1 to allow and 2 to block
+        opt.add_experimental_option("prefs", { 
+            "profile.default_content_setting_values.media_stream_mic": 2, 
+            "profile.default_content_setting_values.media_stream_camera": 2,
+            "profile.default_content_setting_values.geolocation": 2, 
+            "profile.default_content_setting_values.notifications": 2 
+        })
+        self.driver = webdriver.Chrome(chrome_options=opt)
+        self.driver.implicitly_wait(20)
 
     def go_to_tab(self, handler):
         all_handlers = self.driver.window_handles
