c138a3957733f5e1eca4813ab6dac74f0e683cbb
Illya Verbovskyi <illyaverb@gmail.com>
2020-12-30 12:30:06 +0200

    First real try Add minimal work functions for testing in real time

--- bot.py
+++ bot.py
@@ -8,40 +8,103 @@ import classes.Student_In_Meeting as sim
 DEBUG = False
 TOKEN = os.environ['TOKEN']
 ADMIN = ['460390112']
-student = None
+SCHEDULE = {
+    1: ((8, 30), (10, 5)),
+    2: ((10, 25), (12, 0)),
+    3: ((12, 20), (13, 55)),
+    4: ((14, 15), (15, 50)),
+    5: ((16, 10), (17, 45))
+}
+# DB format = CHAT_ID: {'tab_handler':None, 'input_data':None, 'message_mode':False}
+DATABASE = {}
+
+
+def current_lec_ending(time_struct):
+    hour = time_struct.tm_hour
+    minute = time_struct.tm_min
+    week = time_struct.tm_wday
+    
+    lec_ending = list(time_struct)
+    for i, j in SCHEDULE.items():
+        if j[1][0] <= hour and j[1][1] <= minute and week != 6:
+            lec_ending[3] = hour
+            lec_ending[4] = minute
+
+    return time.mktime(tuple(lec_ending))
 
 
 def start(update, context):
     print("start")
-    context.bot.send_message(chat_id=update.effective_chat.id,
+    context.bot.send_message(chat_id=update.effective_chat.id, reply_markup=ReplyKeyboardRemove(),
                              text="Привіт. Я можу під'єднатись під будь яким іменем "+
                              "до конференції на сайті https://bbb.comsys.kpi.ua/\n"+
                              "Просто відправ мені посилання у першому рядку та нік у другому.")
 
 
 def end(update, context):
-    global student
     print('end')
-    if student is not None:
-        student.exit_meeting()
+    if update.effective_chat.id in DATABASE and DATABASE[update.effective_chat.id] is not None:
+        if not student.exit_meeting(DATABASE[update.effective_chat.id]['tab_handler']):
+            context.bot.send_message(chat_id=update.effective_chat.id,
+                                     text="Ви не можете залишити конференцію, бо зараз не знаходитесь у жодній.")
+            
+        DATABASE[update.effective_chat.id] = {'tab_handler': None, 'input_data': None, 'message_mode': False}
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text="Ви залишили конференцію. Було приємно співпрацювати, звертайтесь)")
+    else:
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text="Ви не можете залишити конференцію, бо зараз не знаходитесь у жодній.")
+
+
+def msg_mode(update, context):
+    if update.effective_chat.id in DATABASE and DATABASE[update.effective_chat.id] is not None:
+        DATABASE[update.effective_chat.id]['message_mode'] = not DATABASE[update.effective_chat.id]['message_mode']
+
+        if DATABASE[update.effective_chat.id]['message_mode']:
+            context.bot.send_message(chat_id=update.effective_chat.id,
+                                     text="Ви .")
+
+    else:
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text="Ви не можете працювати з повідомленнями, бо зараз не знаходитесь у конференції.")
 
 
 def go_in(update, context):
     global student
-    input_data = update.message.text.split('\n')
-    print(start, input_data)
-    student = sim.Student_In_Meeting()
-    is_connected = student.go_in_meeting(*input_data)
-    if not is_connected:
-        context.bot.send_message(chat_id=ADMIN[0],
-                             text="ERROR!\n"+str(input_data))
-        context.bot.send_message(chat_id=update.effective_chat.id,
-                             text="На даний момент неможливо підключитись до даної конференції. Спробуйте пізніше.")
-        student.browse_quit()
     
+    # input_data is in format [link on meeting, name for meeting]
+    input_data = update.message.text.split('\n')
+    
+    print('start', input_data)
+    if update.effective_chat.id in DATABASE and DATABASE[update.effective_chat.id][0] is not None:
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text="Ви вже знаходитесь на лекції, для нового підключення вийдіть з минулого " +
+                                      "(команда /end), або дочекайтесь його закінчення.")
+        return
 
+    connecting_answ = student.go_in_meeting(*input_data)
+
+    # false if not active meeting
+    is_connected = connecting_answ['status']
+    if not is_connected:
+        DATABASE[update.effective_chat.id] = {'tab_handler': None, 'input_data': None, 'message_mode': False}
+        
+        context.bot.send_message(chat_id=ADMIN[0],
+                                 text="ERROR!\n"+str(input_data))
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text="На даний момент неможливо підключитись до даної конференції. Спробуйте пізніше.")
+        student.remove_current_tab()
+    else:
+        DATABASE[update.effective_chat.id] = {'tab_handler': connecting_answ['tab_handler'],
+                                              'input_data': input_data,
+                                              'message_mode': False}
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text="Ви успішно приєднались до конференції в режимі слухача, під ніком: " +
+                                      input_data[1])
+        
+    
 def echo(update, context):
-    print(update.message.text, '\n', update.effective_chat.id)    
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text) 
 
 
 updater = Updater(token=TOKEN, use_context=True)
@@ -49,8 +112,10 @@ updater = Updater(token=TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 dispatcher.add_handler(CommandHandler('start', start))
 dispatcher.add_handler(CommandHandler('end', end))
+# dispatcher.add_handler(CommandHandler('message_mode', msg_mode))
 
-dispatcher.add_handler(MessageHandler(Filters.regex('^(https?:\/\/(www\.)?bbb\.comsys\.kpi\.ua\/b\/[\w\-]+)\n([\w\s-]+)$'), go_in))
+dispatcher.add_handler(MessageHandler(
+    Filters.regex(r'^(https?:\/\/(www\.)?bbb\.comsys\.kpi\.ua\/b\/[\w\-]+)\n([\w\s-]+)$'), go_in))
 
 if DEBUG:
     echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
@@ -61,5 +126,9 @@ else:
                           port=int(os.environ.get('PORT', '8443')),
                           url_path=TOKEN)
     updater.bot.set_webhook("https://yurahelpbot.herokuapp.com/" + TOKEN)
+
+# create new active student
+student = sim.Student_In_Meeting()
+#student.new_tab('https://www.google.com/')
 
 updater.idle()
--- classes/Student_In_Meeting.py
+++ classes/Student_In_Meeting.py
@@ -5,60 +5,73 @@ from selenium.common.exceptions import NoSuchElementException
 
 import re
 
+
 class Student_In_Meeting:
     def __init__(self):
         self.driver = webdriver.Chrome()
-        self.driver.implicitly_wait(60)
+        self.driver.implicitly_wait(5)
 
+    def go_to_tab(self, handler):
+        all_handlers = self.driver.window_handles
+        for w in all_handlers:
+            if w == handler:
+                self.driver.switch_to.window(w)
+                return True
+        return False
+
+    def new_tab(self, link):
+        self.driver.execute_script("window.open('{}');".format(link))
+        self.driver.switch_to.window(self.driver.window_handles[-1])
+        return self.driver.current_window_handle
+
+    def remove_current_tab(self, previous_handler):
+        self.driver.execute_script("window.close();")
+        self.driver.switch_to.window(
+            previous_handler if previous_handler in self.driver.window_handles else self.driver.window_handles[0])
+        return True
 
     def go_in_meeting(self, url, name, init_msg=[]):
-        self.driver.get(url)
+        dict_answer = {'tab_handler': self.new_tab(url), 'url': url, 'name': name, 'status': None}
 
         input_id = re.findall("<input required=\"required\" class=\"form-control join-form\".+id=\"(.+)\" />",
                               req.get(url).text)[0]
         name_input = self.driver.find_element_by_id(input_id)
         name_input.send_keys(name, Keys.ENTER)
-
         
         try:
             btn_just_listen = self.driver.find_element_by_xpath("//button[@aria-label='Только слушать']")
         except NoSuchElementException:
-            return False
+            dict_answer['status'] = False
+            return dict_answer
+        
         btn_just_listen.click()
 
         for i, j in init_msg:
             if j:
-                self.send_msg(self, i)
-                #self.driver.find_element_by_id("message-input").send_keys(i, Keys.ENTER)
+                self.send_msg(i)
 
-        #while (True):
-        #    txt = input("Your msg: ")
-        #    if txt == "ESC":
-        #        break
-        #    print("Send:", txt)
-        #    self.driver.find_element_by_id("message-input").send_keys(txt, Keys.ENTER)
-
-        return True
-
+        dict_answer['status'] = True
+        return dict_answer
 
     def send_msg(self, msg):
         self.driver.find_element_by_id("message-input").send_keys(msg, Keys.ENTER)
 
-
-    def exit_meeting(self):
+    def exit_meeting(self, handler):
+        current_handler = self.driver.current_window_handle
+        if not self.go_to_tab(handler):
+            return False
         self.driver.find_element_by_id("tippy-4").send_keys(Keys.ENTER)
         self.driver.find_element_by_xpath("//li[@aria-labelledby='dropdown-item-label-15']").send_keys(Keys.ENTER)
         self.driver.find_element_by_xpath("//button[@description='Выйти из конференции']").send_keys(Keys.ENTER)
-        browse_quit()
-
+        self.remove_current_tab(current_handler)
+        return True
 
     def browse_quit(self):
+        self.browse_quit()
         self.driver.quit()
 
-
-    def check_exists_by_xpath(xpath):
-        return len(webdriver.find_elements_by_xpath(xpath)) > 0
-
+    def check_exists_by_xpath(self, xpath):
+        return len(self.driver.find_elements_by_xpath(xpath)) > 0
 
     def is_ended(self):
-        return check_exists_by_xpath("//button[@description='Выйти из конференции']")
+        return self.check_exists_by_xpath("//button[@description='Выйти из конференции']")
